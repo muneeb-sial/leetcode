48c53c5dd68e2851a2bfef8fbc7f63bc04fcc5a1
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2026-08-31 20:42:22 +0500

    leetcode: 128 optimized

--- Rust/src/_128_longest_consecutive_sequence.rs
+++ Rust/src/_128_longest_consecutive_sequence.rs
@@ -8,23 +8,20 @@ pub struct Solution;
 // @lc code=start
 impl Solution {
     pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
-        let set: std::collections::HashSet<_> = nums.iter().collect();
-        let mut counter = 0;
+        let set: std::collections::HashSet<i32> = nums.into_iter().collect();
+        let mut longest = 0;
 
-        for i in set.clone() {
-            let back_check = i.clone() - 1;
-            
-            if !set.contains(&back_check) {
-                let mut length = 0;
-                let mut front_check = i.clone() + length;
-                while set.contains(&front_check) {
+        for &n in &set {
+            if !set.contains(&(n - 1)) {
+                let mut length = 1;
+                while set.contains(&(n + length)) {
                     length += 1;
-                    front_check += 1;
                 }
-                counter = std::cmp::max(length, counter);
+                longest = longest.max(length);
             }
         }
-        counter
+
+        longest
     }
 }
 // @lc code=end
