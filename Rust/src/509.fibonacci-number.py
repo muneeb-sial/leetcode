4279c81e2be5623e97650843ff10cf19bac23470
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2026-09-07 21:02:17 +0500

    leetcode: 509 done (py)

---
+++ Rust/src/509.fibonacci-number.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=509 lang=python3
+#
+# [509] Fibonacci Number
+#
+
+
+# @lc code=start
+class Solution:
+    def fib(self, n: int) -> int:
+        a, b = 0, 1
+        for _ in range(0, n):
+            a, b = b, a + b
+        return a
+
+
+# @lc code=end
