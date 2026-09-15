10828091481d70732ec63b13da6a8d2f92173548
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2026-09-15 07:10:34 +0500

    leetcode: 875 done (py)

---
+++ Python/src/875.koko-eating-bananas.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=875 lang=python3
+#
+# [875] Koko Eating Bananas
+#
+
+
+# @lc code=start
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        res = r
+
+        while l <= r:
+            m = (l + r) // 2
+            hours = 0
+            for p in piles:
+                hours += math.ceil(p / m)
+
+            if hours <= h:
+                res = min(res,m)
+                r = m - 1
+            else:
+                l = m + 1
+
+        return res
+
+
+# @lc code=end
